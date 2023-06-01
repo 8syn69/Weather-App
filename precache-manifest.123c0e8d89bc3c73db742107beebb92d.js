96dc0271e568d06bfc672fc26528d5bb90fe53f1
8syn <olivergutt05@gmail.com>
2023-06-01 11:22:35 +0200

    Updates

---
+++ precache-manifest.123c0e8d89bc3c73db742107beebb92d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a5609731003789a2b1f327e4d55051",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7e9be2f85c78b2cf5fae",
+    "url": "/weather-app/static/css/main.f5afafc3.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/weather-app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "7e9be2f85c78b2cf5fae",
+    "url": "/weather-app/static/js/main.8ad1c281.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "df959cc69b6e2216895f93e355557fe3",
+    "url": "/weather-app/static/media/blur-background-1187974.df959cc6.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
